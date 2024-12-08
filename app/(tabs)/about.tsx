97d8771332419d43a3bc930ca1370d41bd0a5435
Akiya-Kudo <k.akiya07@gmail.com>
2024-12-08 22:40:53 +0900

    tutorial first commit in sections 4

---
+++ app/(tabs)/about.tsx
@@ -0,0 +1,10 @@
+import { styles } from '@/styles/styles';
+import { Text, View } from 'react-native';
+
+export default function AboutScreen() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>About screen</Text>
+    </View>
+  );
+}
---
+++ styles/styles.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text: {
+    color: "white"
+  },
+  button: {
+    fontSize: 20,
+    color: "pink",
+    textDecorationLine: "underline",
+    padding: 10,
+  }
+})
